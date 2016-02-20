f54995332234c8f98334de0219616313ec5f5a45
Jose Casanova <josescasanova@gmail.com>
2016-02-19 21:25:49 -0500

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,95 @@ import ReactDOM  from 'react-dom';
 import { Router, Route } from 'react-router';
 import { createHistory } from 'history';
 
-import NotFound from './components/NotFound';
-import App from './components/App';
+/**
+ * App
+ */
+class App extends React.Component {
+
+  render() {
+    let { userName } = this.props.params;
+
+    return (
+      <div className="journal__main">
+        <JournalHead userName={userName}/>
+        <JournalBody/>
+        <JournalFooter/>
+      </div>
+    )
+  }
+}
+
+/**
+ * JournalHead
+ */
+class JournalHead extends React.Component {
+  render() {
+    return (
+      <div className="journal__header">
+        <h1>Hi {this.props.userName}!</h1>
+      </div>
+    )
+  }
+}
+
+/**
+ * Journal Body
+ */
+class JournalBody extends React.Component {
+  render() {
+    return (
+      <div className="journal__body">
+      </div>
+    )
+  }
+}
+
+/**
+ * Journal Footer
+ */
+class JournalFooter extends React.Component {
+  render() {
+    return (
+      <div className="journal__footer">
+      </div>
+    )
+  }
+}
+
+/**
+ * Landing Page
+ */
+class LandingPage extends React.Component {
+  goToApp() {
+    console.log('ye');
+  }
+  render() {
+    return (
+      <form className="form__name" onSubmit={this.goToApp}>
+        <h1>What is your first name?</h1>
+        <input type="text" ref="useName" required/>
+        <input type="submit" />
+      </form>
+    )
+  }
+}
+
+/**
+ * Not Found
+ */
+class NotFound extends React.Component {
+  render() {
+    return <h1>Not Found!</h1>
+  }
+}
 
 /*
   Routes
 */
-
 var routes = (
   <Router history={createHistory()}>
-    <Route path="/" component={App}/>
+    <Route path="/" component={LandingPage}/>
+    <Route path="/user/:userName" component={App}/>
     <Route path="*" component={NotFound} />
   </Router>
 )
